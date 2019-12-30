988fcf313b9eba6dfeb4cea9f72acd57ca5638ae
Nick "bass man" Adams <nicholas.adams@mail.utoronto.ca>
2019-12-30 18:54:18 -0500

    Added optimal move method using an optimal move table

---
+++ optimal_move.py
@@ -0,0 +1,64 @@
+"""Calculates the optimal move using the optimal move table 
+text file. Note thatthis variation always uses 2 dice. 
+Move table copied from 
+http://www.durangobill.com/ShutTheBoxExtra/STB2DICE.txt 
+"""
+
+BOX_NUMS = 9
+THE_BOX = list(range(1, BOX_NUMS + 1))
+
+def generate_optimal_move_dict(file: str) -> dict:
+    """Generates a dictionary from the optimal moves table
+
+    """
+    final_dict = {}
+    f = open(file, "r")
+    line = f.readline()
+    while line != '':
+        line_array = line.split()
+        final_dict[line_array[0]] = line_array
+        line = f.readline()
+    return final_dict
+
+def get_optimal_move(available_nums: [int], roll: int, my_dict: dict) -> [int]:
+    """Gets the optimal move for any 2 dice roll.
+    Based on the optimal move table.
+
+    """
+    flipped = get_flipped(available_nums)
+    print("flipped: ", flipped)
+    optimal_move = my_dict[flipped][roll]
+    return str_to_list(optimal_move)
+
+def str_to_list(my_str: str) -> [int]:
+    """Takes a string of numbers and converts
+    it into a list of integers, where each
+    digit is an element.
+    """
+    my_list = []
+    for char in my_str:
+        my_list.append(int(char))
+    return my_list
+
+def get_flipped(available_nums: [int]) -> str:
+    """Gets flipped nums from available nums.
+    Then sorts the nums and puts them into one string
+
+    """
+    flipped_nums = THE_BOX
+    flipped_str = ''
+    for num in available_nums:
+        if num in flipped_nums:
+            flipped_nums.remove(num)
+    for num in flipped_nums:
+        flipped_str += str(num)
+    if flipped_str == '':
+        return '0'
+    return flipped_str
+
+print(get_flipped( [2, 3, 4] ))
+print(get_flipped( [2, 3, 4,5] ))
+#my_dict = generate_optimal_move_dict("optimal_move_table.txt")
+#test_box = [1,2,3,4,5,6,7,8,9]
+#print(get_optimal_move(test_box , 7, my_dict))
+#print(get_optimal_move([1,2,3,4,5,6,9] , 7, my_dict))
--- shuttheboxbot.py
+++ shuttheboxbot.py
@@ -3,6 +3,7 @@
 from typing import Dict
 import best_move_checker as bmc
 import random
+import optimal_move as om
 
 BOX_NUMS = 9
 THE_BOX = list(range(1, BOX_NUMS + 1))
@@ -12,6 +13,7 @@ DICE_VALUES = list(range(1, DICE_SIDES + 1))
 DUMB = 'DUMB'
 SMART = 'SMART'
 RANDOM = 'RANDOM'
+OPTIMAL = 'OPTIMAL'
 
 def game_simulator(starting_box: [int], bot_type: str, num_games: int) -> float:
     """Plays num_games games with using starting_box and bot_type.
@@ -32,6 +34,7 @@ def play_game(starting_box: [int], bot_type: str) -> int:
     while len(my_box) > 0:
         roll = make_roll()
         my_move = choose_move(my_box, bot_type, roll)
+        print(my_box, 'ROLL: ',roll,'    MOVE: ', my_move)
         if not my_move:
             return 0
         else:
@@ -68,6 +71,9 @@ def choose_move(box: [int], my_type: str, roll: int) -> [int]:
             return dumb_moves[random.randint(0, len(dumb_moves) - 1)]
         else:
             return possible_moves[random.randint(0, len(possible_moves) - 1)]
+    if my_type == OPTIMAL:
+       # print('OPTIMAL:', box, "=======", roll)
+        return om.get_optimal_move(box, roll, my_dict)
 
 def compare_best_moves(available_nums: [int], roll: int, bot_type: str, num_sims: int) -> None:
     print("-=-=-=-=-=BEST MOVES=-=-=-=-=-")
@@ -77,3 +83,17 @@ def compare_best_moves(available_nums: [int], roll: int, bot_type: str, num_sims
     for move in bmc.get_diff(bmc.best_moves(available_nums, roll), bmc.possible_moves(available_nums, roll)):
         print("After ", move, " move ", game_simulator(bmc.box_after_move(available_nums, move), bot_type, num_sims))
     return None
+#print(make_roll())
+#print(THE_BOX)
+#print(game_simulator([1,2,3,4,5,6,7,8,9], SMART, 1000))
+my_dict = om.generate_optimal_move_dict("optimal_move_table.txt")
+print(game_simulator([1,2,3,4,5,6,7,8,9], OPTIMAL, 1))
+#print(game_simulator([1,2,3,4,5,6,7,8,9], OPTIMAL, 100))
+#print("after [3,9] move " ,game_simulator([1,2,3,4,5,6,7,8,9], RANDOM, 1000))
+#print("after [4,8] move " ,game_simulator([1,2,3,5,6,7,9], SMART, 1000))
+#print("after [5,7] move " ,game_simulator([1,2,3,4,6,8,9], SMART, 1000))
+#print("after [1,5,6] move " ,game_simulator([2,3,4,7,8,9], SMART, 1000))
+#print("after [1,2,3,6] move " ,game_simulator([4,5,7,8,9], SMART, 1000))
+#compare_best_moves([2,3,5,6,7,9], 8, RANDOM, 999)
+#compare_best_moves([2,3,5,6,7,9], 8, OPTIMAL, 999)
+#print(om.get_optimal_move( [2,4,5,6] , 7 ))
